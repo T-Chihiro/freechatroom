0c23e01566eb2c43150cdd38279f1622d02280bc
T.Chihiro <chihiro@sein.mse.kyutech.ac.jp>
2018-12-10 03:16:35 +0900

    little fix

--- free-chat/config/application.rb
+++ free-chat/config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+ActiveRecord::Base.protected_environments = %w(production staging)
 
 module FreeChat
   class Application < Rails::Application
